b1d1806217ee6f5cc357aca97e5231bcdb08ac75
Ivan <ivanulanovskij143@gmail.com>
2021-05-27 15:24:05 +0500

    added 2 functions for proper operation of the app

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@ import '../css/style.css'
 import '../css/style.scss'
 import defineDate from './defineDate'
 import defineLocation from './defineLocation'
+// так как импортированные функции вызываются в самом файле, здесь их достаточно просто импортировать
 
