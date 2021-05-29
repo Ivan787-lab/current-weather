bae7e267cf3540292be27dd68b65bf1aad94928f
Ivan <ivanulanovskij143@gmail.com>
2021-05-29 14:54:47 +0500

    changed for correct work with manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,8 @@ const plugins = () => {
         new copyWebpackPligin({
             patterns: [
                 { from: path.resolve(__dirname, 'src/assets/'), to: path.resolve(__dirname, 'app/assets') },
-                { from: path.resolve(__dirname, 'src/img/'), to: path.resolve(__dirname, 'app/img') }
+                { from: path.resolve(__dirname, 'src/img/'), to: path.resolve(__dirname, 'app/img') },
+                { from: path.resolve(__dirname, 'src/manifest.json/'), to: path.resolve(__dirname, 'app/manifest.json') }
             ]
         }), 
     ]
